e8eab1bb8437f50a71284c59b6bc59990ad9187d
Tim Gardiner <tsgardiner@windowslive.com>
2018-04-30 23:52:36 +1200

    Enquiry Form Added
    Added front-end css folder
    Added react-form dependency to front-end
    Form CSS errors

---
+++ react-front-end/src/EnquiryForm.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import { Form, Text, TextArea, Select, } from 'react-form'
+
+import './css/EnquiryForm.css'
+
+//TODO Look into making urgent emails as a result of this.
+const enquiryOptions = [
+    {
+      label: 'Urgent',
+      value: 'urgent',
+    },
+    {
+      label: 'Not working',
+      value: 'notworking',
+    },
+    {
+      label: "Thank you",
+      value: 'thankyou',
+    },
+ ]
+
+
+class EnquiryForm extends Component {
+	constructor(props) {
+		super (props)
+		this.state = {}
+
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+	
+
+	handleSubmit(event) {
+		console.log(event) //Shows form data on console
+		//event.preventDefault(); Not working for some reason
+		//const data = new FormData(event.target)	
+
+		this.setState ({ 'submittedValues' : event }) //Set state //Probably not needed
+
+		//console.log(this.state)		
+	}
+
+	
+	render() {
+		return (						
+			<div className='EnquiryForm'>
+				<Form onSubmit={this.handleSubmit}>				
+	    			{formApi => (
+				      	<form onSubmit={formApi.submitForm} id="form">
+				      		
+				      		<label htmlFor="enquiryType" className="d-block">Type of Enquiry: </label>
+					        <Select field="enquiryType" id="enquiryType" options={enquiryOptions} className="mb-4" />
+					       
+					        <label htmlFor="firstName">First name: </label>					        
+					        <Text field="firstName" id="firstName" />
+					        
+					        <label htmlFor="lastName">Last name: </label>
+					        <Text field="lastName" id="lastName" />				        
+					        
+					        <label htmlFor="email">Email Address: </label>
+					        <Text 	
+					        	field="email" id="email" 
+					        	//Might use this might not
+				        		//validate={validate} asyncValidate={emailValidate}
+					        />	
+
+					        <label htmlFor="comments">Comments: </label>
+					        <TextArea field="comments" id="comments" />        
+					        
+					        <button type="submit" className="submitEnquiry">
+					          Submit
+					        </button>
+	      				</form>
+	    			)}
+	  			</Form>		  			
+			</div>	  		
+		)
+	}
+}
+
+export default EnquiryForm
