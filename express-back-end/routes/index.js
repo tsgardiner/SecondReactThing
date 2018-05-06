26ff4a1d004f5a2c6c51b58435a33b14749138e3
Tim Gardiner <tsgardiner@windowslive.com>
2018-05-06 15:03:21 +1200

    Removed commented code

--- express-back-end/routes/index.js
+++ express-back-end/routes/index.js
@@ -27,72 +27,8 @@ router.post('/send', (req, res, next) => {
 	var formData = req.body.formData
 
 	//Create pdf document
-	pdfGenerator.GeneratePDF(formData)
+	pdfGenerator.GeneratePDF(formData)	
 	
-	/*var firstName = req.body.formData.firstName	
-	var lastName = req.body.formData.lastName
-	var name = firstName + ' ' + lastName	
-	var enquiryType = req.body.formData.enquiryType
-	var email = req.body.formData.email
-	var comments = req.body.formData.comments
-	var content = `name: ${name} \n email: ${email} \n  enquiryType: ${enquiryType} \n  message: ${comments} `*/
-
-	//Creating PDF 
-	//The pdf creation and buffer will get moved to generate-pdf once it's working
-	//let doc = new pdfDocument()	
-	//let fileName = firstName + lastName + 'Enquiry' + '.pdf'
-
-	//PDF Content
-	/*doc.text(name, 50, 50)
-	doc.text(email)
-	doc.text(enquiryType)
-	doc.text(comments)*/
-
-	//doc.end()
-
-	/*let buffers = []
-	doc.on('data', buffers.push.bind(buffers))
-	
-	doc.on('end', () => {
-
-		var pdfData = Buffer.concat(buffers)			
-
-		var mail = {
-			from: name,
-			to: 'tsgardinerdevtesting@gmail.com',  //Change to email address that you want to receive messages on
-			subject: enquiryType,
-			text: content,  
-			attachments: [{
-	    		filename: fileName,
-	    		content: pdfData,
-	    		contentType: 'application/pdf'
-			}]
-		}
-
-		transporter.sendMail(mail, (err, data) => {
-			if (err) {
-			  res.json({
-			    msg: 'fail'
-			  })
-			} else {    	
-			  res.json({
-			    msg: 'success'
-			  })
-			}
-		})	
-
-	}) //END OF DOC ON END*/
-	
-
-	/*console.log('')
-	console.log('.........................................................................')
-	console.log('pdfData after doc.on finish')
-	console.log(pdfData)*/
-
-	/*console.log('')
-	console.log('.........................................................................')
-	console.log('pdfData in doc.on')
-	console.log(pdfData)*/
 })
 
 
